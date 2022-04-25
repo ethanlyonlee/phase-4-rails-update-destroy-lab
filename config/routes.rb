44950cb90f9690697b9fe065cf5feed1c51167fe
Ethan Lee <wxliyc@gmail.com>
2022-04-25 16:03:29 -0500

    commit tree 23670c2901beb59cd6f72ddf7b267d3aac799f3a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants, only: [:index, :show, :create, :update, :destroy]
+  patch '/plants/:id/is_in_stock', to: 'plants#is_in_stock'
 end
